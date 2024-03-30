51477bcf07d83721978e9805b423b3d2136b3116
mkpanda91 <mahesh.k.panda91@gmail.com>
2024-03-31 00:59:34 +0530

    Pushing changes to files done as per instruction in Task 7 till point no 12

--- git-test-project/src/main/java/com/guvi/gitproject/CheckEven.java
+++ git-test-project/src/main/java/com/guvi/gitproject/CheckEven.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class CheckEven {
 
     public static void main(String[] args) {
-        int num;
+        //Declaring the variable to store the number entered by user
+        int int_num;
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter a number");
-        num = sc.nextInt();
-        if (num % 2 == 0)
+        int_num = sc.nextInt();
+        if (int_num % 2 == 0)
             System.out.println("It is an Even number");
         else
             System.out.println("It is an Odd number");
